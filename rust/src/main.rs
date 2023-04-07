34b1e697dcf45c1af460a742d63a3ab57507e042
Hadi <hadi.jafar@torontomu.ca>
2023-04-07 19:28:05 -0400

    initial rust war code

---
+++ rust/src/main.rs
@@ -0,0 +1,6 @@
+#![allow(non_snake_case,non_camel_case_types,dead_code)]
+use std::vec::Vec;
+
+#[cfg(test)]
+#[path = "tests.rs"]
+mod tests;
---
+++ rust/target/debug/deps/war-343dcb89b1a32ad7.d
@@ -0,0 +1,6 @@
+/Users/hadijafar/Downloads/war_rs/target/debug/deps/war-343dcb89b1a32ad7: src/main.rs src/tests.rs
+
+/Users/hadijafar/Downloads/war_rs/target/debug/deps/war-343dcb89b1a32ad7.d: src/main.rs src/tests.rs
+
+src/main.rs:
+src/tests.rs:
